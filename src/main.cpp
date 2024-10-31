50107a76d98c0b900bf849e5b16eb692f92ad2ad
Nekeroo <m.grattardpro@gmail.com>
2024-10-31 16:10:53 +0100

    Feature : Ajout Librairie Accelerometre + Detection droite / gauche + mouvement

--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,50 @@
 #include <Arduino.h>
-#include <WiFiManager.h>
-#include <PubSubClient.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_MMA8451.h>
+#include <Adafruit_Sensor.h>
 
-const char* mqtt_server = "broker.emqx.io";
-
-WiFiClient espClient;
-WiFiManager wm;
-PubSubClient clientMqtt(espClient);
-unsigned long lastMsg = 0;
-#define MSG_BUFFER_SIZE	(50)
-char msg[MSG_BUFFER_SIZE];
-int value = 0;
-
-void setup_wifi();
-void callback(char* topic, byte* payload, unsigned int length);
-void reconnect();
-void sendMessage(long now);
+Adafruit_MMA8451 mma = Adafruit_MMA8451();
+float accelZ;
 
 void setup() {
-  wm.resetSettings();
-  Serial.begin(115200);
-  setup_wifi();
-  clientMqtt.setServer(mqtt_server, 1883);
-  clientMqtt.setCallback(callback);
+    Serial.begin(115200);
+    if (! mma.begin()) {
+        Serial.println("Couldnt start");
+        while (1);
+    }
+    Serial.println("MMA8451 found!");
+    sensors_event_t event; 
+    mma.getEvent(&event);
+    accelZ = event.acceleration.z;
 }
 
 void loop() {
 
-  if (WiFi.status() == WL_CONNECTED && !clientMqtt.connected()) {
-    reconnect();
-  }
-  
-  clientMqtt.loop();
+    mma.read();
 
-  unsigned long now = millis();
-  sendMessage(now);
-}
+    delay(2000);
 
-void setup_wifi() {
-
-  wm.autoConnect("BopIoTSpot", "securiteMax");
-
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message reçu [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-
-}
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!clientMqtt.connected()) {
-    Serial.print("Connexion au broker MQTT ...");
-    // Create a random client ID
-    String clientId = "mqttx_";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    if (clientMqtt.connect(clientId.c_str())) {
-      Serial.println("connecté");
-      // Once connected, publish an announcement...
-      clientMqtt.publish("test", "Client connecté : ", clientId);
-      // ... and resubscribe
-      clientMqtt.subscribe("test");
-    } else {
-      Serial.print("Echec, rc=");
-      Serial.print(clientMqtt.state());
-      Serial.println("Retry dans 5 secondes");
-      // Wait 5 seconds before retrying
-      delay(5000);
+    sensors_event_t event; 
+    mma.getEvent(&event);
+    if ((event.acceleration.z > accelZ + 1) || (event.acceleration.z < accelZ - 1)) {
+        Serial.println("IT MOVES");
     }
-  }
-}
 
-
-void sendMessage(long now) {
-  if (now - lastMsg > 2000) {
-    lastMsg = now;
-    ++value;
-    snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
-    Serial.print("Message envoyé: ");
-    Serial.println(msg);
-    clientMqtt.publish("test", msg);
-  }
+    uint8_t o = mma.getOrientation();
+  
+    switch (o) {
+        case MMA8451_PL_PUF: 
+        Serial.println("Portrait Up Front");
+        break;
+        case MMA8451_PL_LRF: 
+        Serial.println("Landscape Right Front");
+        break;
+        case MMA8451_PL_LLF: 
+        Serial.println("Landscape Left Front");
+        break;
+        }
+    Serial.println();
+    delay(500);
+    
 }
